8274b08ed4a65eda63b9800de7e28db4a8effcd0
landaverde1196 <alejndro1196@gmail.com>
2020-07-17 22:25:01 -0600

    future css  code

--- shared.js
+++ shared.js
@@ -4,13 +4,17 @@ let modalNoButton = document.querySelector(".modal__action--negative");
 let selectPlanButtons = document.querySelectorAll(".plan button");
 let toggleButton = document.querySelector(".toggle-button");
 let mobileNav = document.querySelector(".mobile-nav");
+let ctaButton = document.querySelector(".main-nav__item--cta");
 
 for (let i = 0; i < selectPlanButtons.length; i++) {
   selectPlanButtons[i].addEventListener("click", function () {
     // modal.style.display = "block";
     // backdrop.style.display = "block";
     modal.classList.add("open");
-    backdrop.classList.add("open");
+    backdrop.style.display = "block";
+    setTimeout(() => {
+      backdrop.classList.add("open");
+    }, 10);
   });
 }
 
@@ -31,11 +35,29 @@ function closeModal() {
     modal.classList.remove("open");
   }
   backdrop.classList.remove("open");
+  setTimeout(() => {
+    backdrop.style.display = "none";
+  }, 200);
 }
 
 toggleButton.addEventListener("click", function () {
   // mobileNav.style.display = "block";
   // backdrop.style.display = "block";
   mobileNav.classList.add("open");
-  backdrop.classList.add("open");
+  backdrop.style.display = "block";
+  setTimeout(() => {
+    backdrop.classList.add("open");
+  }, 10);
+});
+
+ctaButton.addEventListener("animationStart", function (event) {
+  console.log("Animation Started", event);
+});
+
+ctaButton.addEventListener("animationEnd", function (event) {
+  console.log("Animation ended", event);
+});
+
+ctaButton.addEventListener("animationIteration", function (event) {
+  console.log("Animation iteratation", event);
 });
